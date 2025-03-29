486b0ce8979daa60382bf8c71d2530ba1cec54de
rasi1982 <fathorrasi1982@gmail.com>
2025-03-30 06:18:43 +0700

    Update premi.sh

--- premi.sh
+++ premi.sh
@@ -275,7 +275,7 @@ curl "ipinfo.io/city?token=7a814b6263b02c" > /root/.city
 MYIP=$(curl -sS ipv4.icanhazip.com)
 echo -e "\e[32mloading...\e[0m" 
 clear
-izinsc="https://raw.githubusercontent.com/rasi1982/1z1n/main/L1cense.txt"
+izinsc="https://raw.githubusercontent.com/rasi1982/ijin/main/alpha"
 # USERNAME
 rm -f /usr/bin/user
 username=$(curl $izinsc | grep $MYIP | awk '{print $2}')
